b5c71e48cf53a93aca86261cad6d1410a13436f8
alimuamar11 <alimuamar11@gmail.com>
2021-02-04 10:05:15 +0700

    Inline Styles & StyleSheet Objects

--- App.js
+++ App.js
@@ -4,59 +4,28 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    // <View style={styles.container}>
-    //   <Text>{outputText}</Text>
-    //   <Button title="change text" onPress={() => setOutputText('Tulsan telah diperbarui') }/>
-    // </View>
-        // <View style={{padding: 40}}>
-        //   <View style={{flexDirection: 'column',alignItems: 'flex-start'}}>
-        //     <Button  title="Add"/>
-        //     <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderBottomWidth: 1}}/>
-        //     <Button  title="Add Again"/>
-        //   </View>
-        // </View>
-
-        <View style={{padding: 20, flexDirection: 'row', justifyContent: 'space-around' }}>
-          <View style={{
-            backgroundColor: 'red',
-            width: 100,
-            height: 100,
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-            <Text>1</Text>
-          </View>
-          <View style={{
-            backgroundColor: 'green',
-            width: 100,
-            height: 100,
-            justifyContent: 'flex-end',
-            alignItems: 'center'
-          }}>
-            <Text>2</Text>
-          </View>
-          <View style={{
-            backgroundColor: 'blue',
-            width: 100,
-            height: 100,
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-            <Text>3</Text>
-
-          </View>
-        </View>
-
-
-
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder= "Course Goal" style={styles.input}/>
+        <Button title="ADD" />
+      </View>
+    </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 10
   },
+  inputContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
+
 });
